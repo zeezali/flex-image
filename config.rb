af7bdf17a4d6968456fa890a65e0cac49715eca3
Zee Zali <zee@soapcreative.com>
2013-06-04 18:55:42 +1000

    Added all the assets

---
+++ config.rb
@@ -0,0 +1,25 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "/"
+sass_dir = "sass"
+images_dir = "/"
+javascripts_dir = "js"
+fonts_dir = "css/fonts"
+
+output_style = :expanded
+environment = :development
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass css/sass scss && rm -rf sass && mv scss sass
